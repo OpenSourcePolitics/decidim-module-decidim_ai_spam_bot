afd2dec3fc5040476a660ce7aca501ecc2f3e877
barbara oliveira <barbarafictiovore@gmail.com>
2026-03-12 10:51:29 +0100

    fix CI - test

--- lib/decidim/decidim_ai_spam_bot/test/factories.rb
+++ lib/decidim/decidim_ai_spam_bot/test/factories.rb
@@ -6,7 +6,7 @@ require "decidim/core/test/factories"
 FactoryBot.define do
   factory :decidim_ai_spam_bot_component, parent: :component do
     name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :decidim_ai_spam_bot).i18n_name }
-    manifest_name :decidim_ai_spam_bot
+    manifest_name { :decidim_ai_spam_bot }
     participatory_space { create(:participatory_process, :with_steps) }
   end
 
